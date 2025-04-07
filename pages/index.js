946a06985bf9abd6398e51b561038ac9a90a7a0e
Floyd081995 <realwhizz08@gmail.com>
2025-04-07 15:07:15 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,31 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Flex Space Matchmaker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <h1>Welcome to Flex Space Matchmaker</h1>
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Match freelancers, SMEs, and startups with underutilized offices and flexible spaces today!
         </p>
+        
+        <form name="contact" method="POST" data-netlify="true">
+          <input type="text" name="name" placeholder="Your Name" required />
+          <br />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <br />
+          <button type="submit">Get Started</button>
+        </form>
       </main>
 
-      <Footer />
+      <footer>
+        <p>&copy; 2025 Flex Space Matchmaker. All rights reserved.</p>
+      </footer>
     </div>
-  )
+  );
 }
